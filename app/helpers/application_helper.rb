0c0e01ada3a562d76c5fe306ebb071a6abc6b9c8
quietpochatok <bosroin@gmail.com>
2020-12-05 00:36:57 +0500

    add inclination for question.count

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,18 @@ module ApplicationHelper
       asset_path 'avatar.jpg'
     end
   end
+
+
+  def inclination(count_question)
+    first_word_form, second_word_form, third_word_form = "вопрос", "вопроса", "вопросов"
+
+    remainder_of_division_ten = count_question % 10
+    value =
+        if (count_question % 100).between?(11, 14) then third_word_form
+        elsif remainder_of_division_ten == 1 then first_word_form
+        elsif remainder_of_division_ten.between?(2, 4) then second_word_form
+        else third_word_form
+        end
+    "#{count_question} #{value}"
+  end
 end
