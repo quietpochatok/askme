03e424dcfa52e123910931dc9a8a79753e0d032f
quietpochatok <bosroin@gmail.com>
2020-12-10 14:15:10 +0500

    refactor: delete commit usless code

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,38 +1,9 @@
 class Question < ApplicationRecord
-
   belongs_to :user
   belongs_to :author, class_name: "User", optional: true
 
-  validates :text, :user, presence: true
+  # belongs_to :user дает валидацию на присутствие юзера ниже., поэтмоу
+  # поле юзера не нужно валидировать
+  validates :text, presence: true
   validates :text, length: { maximum: 255 }
-
-  # # Демонтрация жизненного цикла объекта навесили на все популярные коллбэки
-  # # вои методы.
-  # before_validation :before_validation
-  # after_validation :after_validation
-
-  # before_save :before_save
-  # after_save :after_save
-
-  # before_create :before_create
-  # after_create :after_create
-
-  # before_update :before_update
-  # after_update :after_update
-
-  # before_destroy :before_destroy
-  # after_destroy :after_destroy
-
-  # private
-
-  # # Динамически сгенерируем пару методов для каждого действия, используя
-  # # возможности руби (этот код нужен только для демонстрации валидаций и потом
-  # # мы его удалим).
-  # %w(validation save create update destroy).each do |action|
-  #   %w(before after).each do |time|
-  #     define_method("#{time}_#{action}") do
-  #       puts "******> #{time} #{action}"
-  #     end
-  #   end
-  # end
 end
