396b9fa74737bf31dfcb6b2d65f42717de79d9c4
quietpochatok <bosroin@gmail.com>
2020-12-18 12:34:17 +0500

    fix bug logout users into avatar_field

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   # Ресурс пользователей (экшен destroy не поддерживается)
   resources :users
   resources :questions, only: %i[edit update destroy create]
-  resources :sessions, only: %i[new create destroy]
-  get 'sign_up' => 'users#new'
-  get 'log_in' => 'sessions#new'
-  get 'log_out' => 'sessions#destroy'
+  resource :sessions, only: %i[new create destroy]
+
 end
 
