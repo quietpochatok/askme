a7634fef55c3c276abd8dc70bb01d5804f8445ba
quietpochatok <bosroin@gmail.com>
2021-01-24 00:20:19 +0500

    fixed js settings: delete unless libs in pack/application.js

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -4,23 +4,15 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-// require("jquery")
-// import
+
+import '../scripts/click'
+
 import '../styles/application.scss'
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-$(function(){
-  $('#ask-button').click(function(){
-    $('#ask-form').slideToggle(300);
-    return false;
-  });
-});
 
 const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
