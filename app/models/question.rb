78ecd6f78a6bf16ccfcd9c69297b7b4dc862bf54
quietpochatok <bosroin@gmail.com>
2021-03-19 18:31:47 +0500

    Add HashtagQuestion model; migration many-to-many

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,9 @@ class Question < ApplicationRecord
   belongs_to :user
   belongs_to :author, class_name: "User", optional: true
 
+  has_many :hashtag_questions, dependent: :destroy
+  has_many :hashtags, through: :hashtag_question
+
   # belongs_to :user дает валидацию на присутствие юзера ниже., поэтмоу
   # поле юзера не нужно валидировать
   validates :text, presence: true
