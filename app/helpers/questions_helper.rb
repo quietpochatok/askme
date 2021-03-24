4436234810f4a8deab3c9578d6a7c61ea64e337e
quietpochatok <bosroin@gmail.com>
2021-03-24 22:03:23 +0500

    Add def in question_helper, which change text on link

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,7 @@
 module QuestionsHelper
+  def link_hashtag_in_question(text)
+    text.gsub(Hashtag::HASHTAG_REGEXP) do |ht|
+      link_to ht, hashtag_path(ht.delete('#').downcase )
+    end.html_safe
+  end
 end
